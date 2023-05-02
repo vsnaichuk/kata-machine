65f9743199c7316a71150988180cbeb0af66555f
vsnaichuk <v.snaichuk@gmail.com>
2023-05-03 00:22:38 +0300

    Practice Bubble Sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,15 +1,8 @@
-// swap the elements in the first and second index provided
-function swap(arr: number[], first: number, second: number) {
-   let temp = arr[first]
-    arr[first] = arr[second]
-    arr[second] = temp
-}
-
 export default function bubble_sort(arr: number[]): void {
   for (let i = 0; i < arr.length; ++i) {
-    for (let k = 0; k < arr.length - 1 - i; ++k) {
+    for (let k = 0; k < arr.length - i - 1; ++k) {
       if (arr[k] > arr[k + 1]) {
-        swap(arr, k, k+1)
+        [arr[k], arr[k + 1]] = [arr[k + 1], arr[k]]
       }
     }
   }
